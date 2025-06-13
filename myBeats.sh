39e5494dbab3e51f9c9af7049e72cd3acbd05e8e
KhagendraN <nkhagendra1@gmail.com>
2025-06-13 08:08:59 +0545

    updated

--- myBeats.sh
+++ myBeats.sh
@@ -111,7 +111,6 @@ stop_music() {
   mpv_pids=$(pgrep -x mpv)
 
   if [ -n "$mpv_pids" ]; then
-    # Get the PID of the mpv process used by mpvpaper (using the unique argument added)
     mpvpaper_pid=$(ps aux | grep -- 'unique-wallpaper-process' | grep -v 'grep' | awk '{print $2}')
 
     for pid in $mpv_pids; do
